4ca59013e5db86663ecd6253f5a9eaace1c563fa
IliasElab <elab.ilias@gmail.com>
2022-10-08 21:26:22 +0200

    Data array and UseEffect for sudoku

--- src/components/Sudoku.js
+++ src/components/Sudoku.js
@@ -1,46 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const Sudoku = () => {
-    const [data, setData] = useState([])
-    const nb_of_input = 81
+    const [data, setData] = useState([]);
+    const [solution, setSolution] = useState([])
+    const [firstRender, setfirstRender] = useState(true)
+    const nb_of_input = 81;
 
-    function isValid() {
-        let rows = [new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set()];
-        let cols = [new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set()];
-        let squares = [new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set(), new Set()];
-        
-        for (let i = 0; i < 9; i++)
-            for (let j = 0; j < 9; j++)
-                const number = board[i + 9*j]
-                if (number != 0){
-                    if (number in rows[i] or number in cols[j] or number in squares[~~(i / 3) * 3 + ~~(j / 3)]){
-                        return False
-                    }    
-                    rows[i].add(number)
-                    cols[j].add(number)
-                    squares[(i // 3) * 3 + j // 3].add(number)
-                }
+    //When the page is rendered for the first time, change firstRender value
+    useEffect(() => {
+        setfirstRender(false);
+    }, []) 
 
-        return True
-    }
+    useEffect(() => {
+        //Avoid the useEffect to load if it is the first Render of the component
+        if (!firstRender){
+            console.log(data)   
+        } 
+    }, [data])
+    
+    useEffect(() => {
+        if (!firstRender){
+            console.log(solution);
+        }    
+    }, [solution])
 
-    function solving() {
-
+    function nextSlot(board) {
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+                if (board[i][j] === 0) 
+                    return [i, j, false];
+            }
+        }
+        return [-1, -1, true];
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-        let filled_Sudoku = []
-        for (let i = 0; i < nb_of_input; i++) {
-            if (e.target[i].value > 0 && e.target[i].value < 10){
-                filled_Sudoku.push(e.target[i].value)
-            }
-            else{
-                filled_Sudoku.push(0)
+        let filled_Sudoku = [[], [], [], [], [], [], [], [], []];
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                let val = parseInt(e.target[i * 9 + j].value)
+                if (val > 0 && val < 10){
+                    filled_Sudoku[i].push(val);
+                }
+                else{
+                    filled_Sudoku[i].push(0);
+                }
             }
         }
-        setData(filled_Sudoku)
+        setData(filled_Sudoku);  
     }
 
     const render_inputs = () => {
@@ -59,7 +68,6 @@ const Sudoku = () => {
                     {render_inputs()}
                     <input type='submit' value='Solve'/>
                 </form>
-                {data}
             </div>
         </div>
     );
