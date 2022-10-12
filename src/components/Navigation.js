7c427eb6621845e84d6a97f54f3d44e58ff1f8f2
IliasElab <elab.ilias@gmail.com>
2022-10-12 22:51:18 +0200

    Basic CSS - Centering Sudoku and buttons + About

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
     return (
         <div className="navigation">
-            <NavLink to="/" className='navlink'><img src="/home.png" alt="Home" width="60" height="60"/></NavLink>
+            <NavLink to="/" className='navlink'>Home</NavLink>
             <NavLink to="/about" className='navlink'>About</NavLink>
         </div>
     );
