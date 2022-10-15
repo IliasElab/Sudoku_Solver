d35ee9062c0f92b76cecac696b109b33d7fc7fd2
IliasElab <elab.ilias@gmail.com>
2022-10-15 17:33:34 +0200

    Home About + CSS Desktop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import Sudoku from '../components/Sudoku';
 const Home = () => {
     return (
         <div id="home">
-            <script src="https://kit.fontawesome.com/a3c07ac5df.js" crossorigin="anonymous"></script>
+            <script src="https://kit.fontawesome.com/a3c07ac5df.js" crossOrigin="anonymous"></script>
             <section id="intro">
                 <img src="/AI.jpg" alt="Error in Background"/>
                 <h1>Sudoku Solver</h1>
@@ -19,7 +19,16 @@ const Home = () => {
                 <Sudoku />
             </section>
 
+            <section id='about'>
+                <h4>This Website in made by : <span>Ilias El-Abbassi</span></h4>
+                <h5>Software Engineer</h5>
 
+                <ul id="list_contact">
+                    <li className="list_element"><a href="mailto:elab.ilias@gmail.com"><i className="fa-solid fa-paper-plane"></i> My Mail</a></li>
+                    <li className="list_element"><a href="https://www.linkedin.com/in/ilias-el-abbassi-a161131b5/"><i className="fa-brands fa-linkedin"></i> My LinkedIn</a></li>
+                    <li className="list_element"><a href="https://github.com/IliasElab"><i className="fa-brands fa-github"></i> My Github</a></li>
+                </ul>
+            </section>
             
         </div>
     );
