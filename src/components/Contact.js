ae5538ef286befef11815cd3a2a7cab6a4cd45e6
IliasElab <elab.ilias@gmail.com>
2022-11-12 00:12:40 +0100

    Handle of Animations

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
 
     const contacts = {
         'mail' : {
+            'id' : 'contact_1',
             'link' : "mailto:elab.ilias@gmail.com",
             'icon' : "fa-solid fa-paper-plane",
             'content' : 'My Mail'
         },
 
         'linkedin' : {
+            'id' : 'contact_2',
             'link' : "https://www.linkedin.com/in/ilias-el-abbassi-a161131b5/",
             'icon' : "fa-brands fa-linkedin",
             'content' : 'My LinkedIn'
         },
 
         'github' : {
+            'id' : 'contact_3',
             'link' : "https://github.com/IliasElab",
             'icon' : "fa-brands fa-github",
             'content' : 'My Github'
@@ -26,14 +29,13 @@ const Contact = () => {
         let elements = [];
 
         Object.values(contacts).forEach((element) => {
-            elements.push(<li className="list_element"><a href={element.link}><i className={element.icon}></i> {element.content}</a></li>)
+            elements.push(<li key={element.id} className="list_element"><a href={element.link}><i className={element.icon}></i> {element.content}</a></li>)
         })
-
         return elements;
     }
 
     return (
-        <ul id="list_contact" className='reveal-X'>
+        <ul id="list_contact" className={props.animation ? 'reveal-X active' : 'reveal-X'}>
             {render_contacts()}
         </ul>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sudoku from '../components/Sudoku';
 import Contact from '../components/Contact';
 
 const Home = () => {
 
-    function scrollReveal() {      
-        const finger = document.getElementById('fingerDiv')
-        const sudokuElement = document.querySelector('.reveal-Y')
-        const contactList = document.querySelector('.reveal-X')
+    const [animatedSudoku, setAnimatedSudoku] = useState(false);
+    const [animatedContacts, setAnimatedContacts] = useState(false);
+    const [animatedFinger, setAnimatedFinger] = useState(true);
 
+    function scrollReveal() {
         if(window.scrollY > 0.8 * (document.documentElement.scrollHeight - window.innerHeight)) {
-            contactList.classList.add('active')     
+            setAnimatedContacts(true);
             window.removeEventListener("scroll", scrollReveal);      
         }
-        else if(window.scrollY > 0.9 * window.innerHeight){
-            finger.classList.add('scrolled')
-        }
-        else if(window.scrollY > 0.6 * window.innerHeight) {
-            sudokuElement.classList.add('active') 
-        }
+        else if(window.scrollY > 0.9 * window.innerHeight) { setAnimatedFinger(false); }
+        else if(window.scrollY > 0.6 * window.innerHeight) { setAnimatedSudoku(true); }
     }
+
     window.addEventListener('scroll', scrollReveal)  
 
     return (
         <div id="home">
             <section id="intro">
                 <img src="/AI.jpg" alt="Error in Background"/>
-                <h1>Sudoku Solver</h1>
-                <h3>Sudokus can be fun but sometimes quite frustrating</h3>
+                <h1>Sudoku Game</h1>
+                <h3>Sudoku is a logic-based, number-placement puzzle. The objective is to fill a 9 × 9 grid </h3>
                 <h3><span>Javascript</span> can help us to solve the Sudoku we are stuck on</h3>
-                <div id='fingerDiv'><i className="fa-solid fa-hand-point-down"></i></div>
+                <div id='fingerDiv' className={animatedFinger ? '' : 'scrolled'}><i className="fa-solid fa-hand-point-down"></i></div>
             </section>
+
             <section id="main">
-                <h3 id='left_h3'>Enter your Sudoku and let Javascript find the solution for you !</h3>
-                <div className='reveal-Y'><Sudoku/></div>
+                <h3 id='left_h3'>Enter your Sudoku or try to solve one !</h3>
+                <Sudoku animation={animatedSudoku}/>
             </section>
+
             <section id='about'>
                 <h2>This Website in made by : <span>Ilias El-Abbassi</span></h2>
                 <h3>Software Engineer</h3>
-
-                <Contact/>
+                <Contact animation={animatedContacts}/>
             </section>
         </div>
     );
